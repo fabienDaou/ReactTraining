5678477cd0e13ee0e3f737bf32fcde00f92f407b
Daoulas <d-fabien.daoulas@enablon.com>
2019-04-03 16:58:44 +0200

    Change to FunctionComponent.

--- src/components/ComponentShowCaser.tsx
+++ src/components/ComponentShowCaser.tsx
@@ -1,18 +1,15 @@
-import React, { Component } from "react";
+import React, { FunctionComponent } from "react";
 import { ShowCasedComponent } from "../ComponentContext";
 
-class ComponentShowCaser extends Component<ShowCasedComponent, {}> {
-  render() {
-    const ShowCasedComponent = React.lazy(() =>
-      import("./" + this.props.path)
-    );
-
-    return (
-      <React.Suspense fallback="loading">
-        <ShowCasedComponent />
-      </React.Suspense>
-    );
-  }
-}
+const ComponentShowCaser: FunctionComponent<ShowCasedComponent> = ({
+  path
+}) => {
+  const ShowCasedComponent = React.lazy(() => import("./" + path));
+  return (
+    <React.Suspense fallback="loading">
+      <ShowCasedComponent />
+    </React.Suspense>
+  );
+};
 
 export default ComponentShowCaser;
