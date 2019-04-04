f435e9df0e65529d080b55645f63d1d942d5772f
Daoulas <d-fabien.daoulas@enablon.com>
2019-04-04 15:42:11 +0200

    Change SideMenu to a function component.

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { FunctionComponent } from "react";
 import Drawer from "@material-ui/core/Drawer";
 import Divider from "@material-ui/core/Divider";
 import List from "@material-ui/core/List";
@@ -10,45 +10,46 @@ import { ShowCasedComponent, ShowCaseApplicationState } from "../state/store";
 import { deleteComponent } from "../state/actions";
 
 interface SideMenuProps {
-  componentList: ShowCasedComponent[];
+  components: ShowCasedComponent[];
   deleteComponent(id: number): void;
 }
 
-class SideMenu extends Component<SideMenuProps> {
-  render() {
-    return (
-      <Drawer variant="permanent" anchor="left">
-        <Route
-          render={({ history }) => (
-            <Fab
-              variant="extended"
-              color="primary"
-              aria-label="Add"
-              onClick={() => history.push("/showcase/add")}
-            >
-              Add
-            </Fab>
-          )}
-        />
-        <Divider />
-        <List>
-          {this.props.componentList.map(item => (
-            <Link to={`/showcase/${item.name}`}>
-              <SideMenuElement
-                {...item}
-                onDelete={() => this.props.deleteComponent(item.id)}
-              />
-            </Link>
-          ))}
-        </List>
-      </Drawer>
-    );
-  }
-}
+const SideMenu: FunctionComponent<SideMenuProps> = ({
+  components,
+  deleteComponent
+}) => {
+  return (
+    <Drawer variant="permanent" anchor="left">
+      <Route
+        render={({ history }) => (
+          <Fab
+            variant="extended"
+            color="primary"
+            aria-label="Add"
+            onClick={() => history.push("/showcase/add")}
+          >
+            Add
+          </Fab>
+        )}
+      />
+      <Divider />
+      <List>
+        {components.map(item => (
+          <Link to={`/showcase/${item.name}`}>
+            <SideMenuElement
+              {...item}
+              onDelete={() => deleteComponent(item.id)}
+            />
+          </Link>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
 
 const mapStateToProps = (state: ShowCaseApplicationState) => {
   return {
-    componentList: state.components
+    components: state.components
   };
 };
 
