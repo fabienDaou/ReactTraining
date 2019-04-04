6ae8295061767c36e41c25a161869cda13a3accb
Daoulas <d-fabien.daoulas@enablon.com>
2019-04-04 10:14:31 +0200

    Add basic login page and routing.

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import AppRouter from "./AppRouter";
 import Cat from "./components/Cat";
 import Mouse from "./components/Mouse";
 import ShowCaseApplication from "./components/ShowCaseApplication";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Login from "./components/Login";
 
 export default () => (
   <AppRouter>
     <div className="App">
-      <ShowCaseApplication />
+      <Switch>
+        <Route path="/showcase" component={ShowCaseApplication} />
+        <Route exact path="/login" component={Login} />
+        <Redirect path="/" to="/login" />
+      </Switch>
       <Mouse render={({ x, y }) => <Cat x={x} y={y} />} />
     </div>
   </AppRouter>
